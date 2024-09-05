39137ac5f11e33e61ec3315ca11892c44486f542
NORIKIM <jinagold.k@gmail.com>
2024-09-05 09:24:41 +0900

    프로젝트 생성

---
+++ SwiftUICalculator/SwiftUICalculator/SwiftUICalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICalculatorApp.swift
+//  SwiftUICalculator
+//
+//  Created by 인터웍스_맥북 on 9/5/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
